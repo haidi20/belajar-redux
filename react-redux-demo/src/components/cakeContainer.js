5cbbc812bcd7294bac037bb14e8ac031a13eda69
haidi <haidinurhadinata@gmail.com>
2020-01-02 20:56:19 +0800

    tutorial 14 selesai

---
+++ react-redux-demo/src/components/cakeContainer.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+function cakeContainer() {
+    return(
+        <div>
+            <h2>Number of cakes</h2>
+            <button>Buy cake</button>
+        </div>
+    )
+}
+
+export default cakeContainer;
