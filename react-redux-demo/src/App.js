659b3716f59964f5450679a815040e2a65567bf6
haidi <haidinurhadinata@gmail.com>
2020-01-03 15:04:01 +0800

    tutorial 23 selesai

--- react-redux-demo/src/App.js
+++ react-redux-demo/src/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
+import {Provider} from 'react-redux';
+import store from './redux/store';
+import CakeContainer from './components/CakeContainer';
+import HooksCakeContainer from './components/HooksCakeContainer';
+import IceCreamContainer from './components/IceCreamContainer';
+
 import './App.css';
-import CakeContainer from './components/cakeContainer';
 
 function App() {
   return (
-    <div className="App">
-      <CakeContainer />
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <HooksCakeContainer />
+        <CakeContainer />
+        <IceCreamContainer />
+      </div>
+    </Provider>
   );
 }
 
--- react-redux-demo/src/components/cakeContainer.js
+++ react-redux-demo/src/components/cakeContainer.js
@@ -1,12 +1,29 @@
-import React, {Component} from 'react';
+import React from 'react';
+import {connect} from 'react-redux';
+import {buyCake} from '../redux';
 
-function cakeContainer() {
+function cakeContainer(props) {
     return(
         <div>
-            <h2>Number of cakes</h2>
-            <button>Buy cake</button>
+            <h2>Number of cakes biasa - {props.numOfCakes}</h2>
+            <button onClick={props.buyCake}>Buy cake</button>
         </div>
     )
 }
 
-export default cakeContainer;
+const mapStateToProps = state => {
+    return{
+        numOfCakes: state.cake.numOfCakes
+    }
+}
+
+const mapDispatchToProps = dispatch => {
+    return{
+        buyCake: () => dispatch(buyCake())
+    }
+}
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(cakeContainer);
