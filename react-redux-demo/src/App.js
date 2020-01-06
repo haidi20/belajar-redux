4e1f05ebd0e2f7c75d3444acce13d80dec964457
haidi <haidinurhadinata@gmail.com>
2020-01-06 08:19:08 +0800

    tutorial 26 selesai

--- react-redux-demo/src/App.js
+++ react-redux-demo/src/App.js
@@ -5,6 +5,7 @@ import CakeContainer from './components/CakeContainer';
 import HooksCakeContainer from './components/HooksCakeContainer';
 import IceCreamContainer from './components/IceCreamContainer';
 import NewCakeContainer from './components/NewCakeContainer';
+import ItemContainer from './components/ItemContainer';
 
 import './App.css';
 
@@ -12,6 +13,8 @@ function App() {
   return (
     <Provider store={store}>
       <div className="App">
+        <ItemContainer cake />
+        <ItemContainer />
         <HooksCakeContainer />
         <CakeContainer />
         <IceCreamContainer />
